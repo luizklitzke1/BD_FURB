9443efc0f144d2d9e5c13e895b03fcc23b57443f
luizklitzke1 <luiz.klitzke1@gmail.com>
2023-05-28 18:57:32 -0300

    Ajustada C

--- Atv_Procedures_triggers/consulta.sql
+++ Atv_Procedures_triggers/consulta.sql
@@ -6,15 +6,15 @@
 #Utilize o usuário logado na seção (função user()) para saber o usuário que executou a operação. 
 #Importante: nos casos que envolve alterações (update), registrar no logo o valor antigo e o novo valor (alterado);
 
-CREATE TABLE IF NOT EXISTS log_operacoes_clienteclientecliente
+CREATE TABLE IF NOT EXISTS log_operacoes_cliente
 (
-	dt_operacao DATE        NOT NULL,
-	hr_operacao TIME        NOT NULL,
-	ds_operacao VARCHAR(10) NOT NULL,
-	nm_usuario  VARCHAR(30) NOT NULL,
-	cd_cliente  INT         NOT NULL,
-	vl_antigo   VARCHAR(50)         ,
-   vl_novo     VARCHAR(50)
+	dt_operacao DATE         NOT NULL,
+	hr_operacao TIME         NOT NULL,
+	ds_operacao VARCHAR(10)  NOT NULL,
+	nm_usuario  VARCHAR(30)  NOT NULL,
+	cd_cliente  INT          NOT NULL,
+	vl_antigo   VARCHAR(250)         ,
+	vl_novo     VARCHAR(250)
 );
 
 DELIMITER $$
@@ -22,17 +22,17 @@ CREATE OR REPLACE TRIGGER tg_insercao_cliente AFTER INSERT ON atv_procedures.cli
 FOR EACH ROW
 BEGIN 
 	INSERT INTO log_operacoes_cliente (dt_operacao, hr_operacao, ds_operacao, nm_usuario, cd_cliente     )
-					               VALUES (CURDATE()  , CURTIME()  , "INSERT"   , USER()    , NEW.CD_CLIENTE );
+										VALUES (CURDATE()  , CURTIME()  , "INSERT"   , USER()    , NEW.CD_CLIENTE );
 END $$    
 
-
-##Como logar o que mudou???
 DELIMITER $$
 CREATE OR REPLACE TRIGGER tg_update_cliente AFTER UPDATE ON atv_procedures.cliente
 FOR EACH ROW
 BEGIN 
 	INSERT INTO log_operacoes_cliente (dt_operacao, hr_operacao, ds_operacao, nm_usuario, cd_cliente     , vl_antigo, vl_novo)
-					               VALUES (CURDATE()  , CURTIME()  , "UPDATE"   , USER()    , OLD.CD_CLIENTE , OLD,     , NEW    );
+					               VALUES (CURDATE()  , CURTIME()  , "UPDATE"   , USER()    , OLD.CD_CLIENTE ,
+										        CONCAT("cd_cliente = ", OLD.cd_cliente, ", nm_cliente = ", OLD.nm_cliente, "ds_email = ", OLD.ds_email, ", nr_telefone = ", OLD.nr_telefone), 
+												  CONCAT("cd_cliente = ", NEW.cd_cliente, ", nm_cliente = ", NEW.nm_cliente, "ds_email = ", NEW.ds_email, ", nr_telefone = ", NEW.nr_telefone));
 END $$   
 
 DELIMITER $$
@@ -58,7 +58,7 @@ BEGIN
   SELECT COUNT(*) INTO qtd_reservas
   FROM reserva AS r
   WHERE r.CD_CLIENTE = cd_cliente AND r.NR_QUARTO = nr_quarto
-    AND r.DT_ENTRADA = dt_entrada AND r.DT_ENTRADA + r.QT_DIARIAS <= DT_SAIDA
+    AND r.DT_ENTRADA = dt_entrada AND DATE_ADD(r.DT_ENTRADA, INTERVAL r.QT_DIARIAS - 1 DAY) >= dt_saida;
   
   IF qtd_reservas > 0 THEN
     INSERT INTO HOSPEDAGEM (DT_ENTRADA, DT_SAIDA, FL_SITUACAO, CD_CLIENTE, CD_FUNCIONARIO, NR_QUARTO)
@@ -66,10 +66,39 @@ BEGIN
   END IF;
 END $$
 
-CALL pc_adicionar_hospedagem(1, 100, '2023-05-28', '2023-05-31');
+CALL pc_adicionar_hospedagem(1, 101, '2023-05-28', '2023-05-30');
 
 #d) criar uma rotina para adicionar um serviço a uma determinada hospedagem. Considerar os seguintes parâmetros: identificação da hospedagem e serviço.
 #Atenção para a data de solicitação que deve ser a atual e o número de sequência 
 #(que deve seguir incremental apenas dentro do número da hospedagem, ou seja, este número é zerado para cada nova hospedagem);
 
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE pc_adicionar_servico_hospedagem(IN cd_hospedagem INT, IN cd_servico INT)
+BEGIN
+  DECLARE nr_sequencia_atual INT;
+  
+  SELECT MAX(os.NR_SEQUENCIA) INTO nr_sequencia_atual
+  FROM hospedagem_servico AS os
+  WHERE os.CD_HOSPEDAGEM = cd_hospedagem AND os.CD_SERVICO = cd_servico;
+  
+  IF nr_sequencia_atual IS NULL THEN
+		SET nr_sequencia_atual = 0;
+  END IF;
+  
+  INSERT INTO hospedagem_servico (CD_HOSPEDAGEM, CD_SERVICO, NR_SEQUENCIA, DT_SOLICITACAO)
+  VALUES (cd_hospedagem, cd_servico, nr_sequencia_atual + 1, CURRENT_DATE);
+END $$
+
+CALL pc_adicionar_servico_hospedagem(1, 1);
+
 #e) criar uma rotina para mudar o status (coluna fl_situacao) para 'F' - finalizada. Esta rotina deverá receber como parâmetro o identificador da hospedagem. 
+
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE pc_finalizar_hospedagem (IN cd_hospedagem INT)
+BEGIN
+  UPDATE hospedagem AS h
+  SET h.FL_SITUACAO = 'F'
+  WHERE h.CD_HOSPEDAGEM = cd_hospedagem;
+END $$
+
+CALL pc_finalizar_hospedagem(1);
