7fdbc94dc32fa1a5ab06686f74c4e811aeb8631f
luizklitzke1 <luiz.klitzke1@gmail.com>
2023-05-29 21:44:48 -0300

    Adicionada resolução da B

--- Atv_Procedures_triggers/consulta.sql
+++ Atv_Procedures_triggers/consulta.sql
@@ -44,6 +44,17 @@ BEGIN
 END $$ 
 
 #b) criar uma rotina que sinalize (liste) a disponibilidade de quarto(s), ou seja sem reserva, considerando uma determinada data passada com parâmetro;
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE pc_lista_quartos_disponiveis(IN dt_disponivel DATE)
+BEGIN
+    SELECT q.NR_QUARTO
+    FROM quarto q
+    LEFT JOIN reserva r ON q.NR_QUARTO = r.NR_QUARTO
+    WHERE r.NR_RESERVA IS NULL OR (dt_disponivel NOT BETWEEN r.DT_ENTRADA AND DATE_ADD(r.DT_ENTRADA, INTERVAL r.QT_DIARIAS - 1 DAY))
+    ORDER BY q.NR_QUARTO ASC;
+END $$
+
+CALL pc_lista_quartos_disponiveis(DATE '2023-05-28');
 
 #c) criar uma rotina para adicionar uma hospedagem passando como parâmetros: cliente (o seu cadastro, ou seja, seu id/nome), quarto, data de entrada e 
 #data prevista de saída. Utilizar cd_funcionario (1) visto que não teremos controle de autenticação, e fl_situacao ('O') para ocupado.  
@@ -61,7 +72,7 @@ BEGIN
     AND r.DT_ENTRADA = dt_entrada AND DATE_ADD(r.DT_ENTRADA, INTERVAL r.QT_DIARIAS - 1 DAY) >= dt_saida;
   
   IF qtd_reservas > 0 THEN
-    INSERT INTO HOSPEDAGEM (DT_ENTRADA, DT_SAIDA, FL_SITUACAO, CD_CLIENTE, CD_FUNCIONARIO, NR_QUARTO)
+    INSERT INTO hospedagem (DT_ENTRADA, DT_SAIDA, FL_SITUACAO, CD_CLIENTE, CD_FUNCIONARIO, NR_QUARTO)
     VALUES (dt_entrada, dt_saida, 'O', cd_cliente, 1, nr_quarto);
   END IF;
 END $$
@@ -89,10 +100,12 @@ BEGIN
   VALUES (cd_hospedagem, cd_servico, nr_sequencia_atual + 1, CURRENT_DATE);
 END $$
 
+CALL pc_adicionar_servico_hospedagem(2, 3);
 CALL pc_adicionar_servico_hospedagem(1, 1);
+CALL pc_adicionar_servico_hospedagem(2, 2);
+CALL pc_adicionar_servico_hospedagem(2, 3);
 
 #e) criar uma rotina para mudar o status (coluna fl_situacao) para 'F' - finalizada. Esta rotina deverá receber como parâmetro o identificador da hospedagem. 
-
 DELIMITER $$
 CREATE OR REPLACE PROCEDURE pc_finalizar_hospedagem (IN cd_hospedagem INT)
 BEGIN
@@ -101,4 +114,4 @@ BEGIN
   WHERE h.CD_HOSPEDAGEM = cd_hospedagem;
 END $$
 
-CALL pc_finalizar_hospedagem(1);
+CALL pc_finalizar_hospedagem(5);
